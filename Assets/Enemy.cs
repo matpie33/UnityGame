02d27a49c135f82607495e23f8c61f9dd3f95324
Mateusz <mateusz.pietal@gmail.com>
2023-09-03 21:53:15 +0200

    Make the wolves chase player and do attack animation.

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -7,15 +7,47 @@ public class Enemy : MonoBehaviour
 {
     private EnemyState enemyState = new EnemyState();
 
+    private CharacterController characterController;
+    private readonly float minimumDistanceToChase = 10;
+    private Animator animator;
+    private float minimumDistanceToAttack = 2;
+
     public NavMeshAgent navMeshAgent { get; private set; }
 
     private void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        characterController = FindObjectOfType<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     public void DecreaseHealth(int byValue)
     {
         enemyState.DecreaseHealth(byValue);
+    }
+
+    private void Update()
+    {
+        Vector3 playerPosition = characterController.transform.position;
+        float distance = Vector3.Distance(navMeshAgent.transform.position, playerPosition);
+        if (distance < minimumDistanceToChase)
+        {
+            navMeshAgent.SetDestination(playerPosition);
+            if (distance < minimumDistanceToAttack)
+            {
+                animator.SetBool(WolfAnimationVariables.IS_ATTACKING, true);
+                animator.SetBool(WolfAnimationVariables.IS_CHASING, false);
+            }
+            else
+            {
+                animator.SetBool(WolfAnimationVariables.IS_CHASING, true);
+                animator.SetBool(WolfAnimationVariables.IS_ATTACKING, false);
+            }
+        }
+        else
+        {
+            animator.SetBool(WolfAnimationVariables.IS_CHASING, false);
+            navMeshAgent.ResetPath();
+        }
     }
 }
---
+++ Assets/constants/WolfAnimationVariables.cs
@@ -0,0 +1,8 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class WolfAnimationVariables
+{
+    public const string IS_CHASING = "isChasing";
+    public const string IS_ATTACKING = "isAttacking";
+}
