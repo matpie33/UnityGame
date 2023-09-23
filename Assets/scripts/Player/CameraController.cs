02345d6174f1c34d6f4d5f3a0889dc0b99443fe6
Mateusz <mateusz.pietal@gmail.com>
2023-09-23 17:35:53 +0200

    Refactor camera controller - rename variables.

--- Assets/scripts/Player/CameraController.cs
+++ Assets/scripts/Player/CameraController.cs
@@ -8,53 +8,49 @@ public class CameraController : Observer
 {
     [Header("Framing")]
     [SerializeField]
-    private Camera _camera = null;
+    private new Camera camera = null;
 
     [SerializeField]
-    private Transform _followTransform = null; //
+    private Transform playerTransform = null;
 
-    private Vector3 planarDirection; //cameras forward on xz plane
-    private Quaternion _targetRotation;
+    private Vector3 planarDirection; //camera's forward on xz plane
+    private Quaternion targetRotation;
 
     [Header("Rotation")]
     [SerializeField]
     [Range(-90, 90)]
-    private float _minVerticalAngle = -90;
+    private float minVerticalAngle = -90;
 
     [Range(-90, 90)]
     [SerializeField]
-    private float _maxVerticalAngle = 90;
+    private float maxVerticalAngle = 90;
 
     [Header("Distance")]
     [SerializeField]
-    private float _zoomSpeed = 10f;
+    private float zoomSpeed = 10f;
 
     [SerializeField]
-    private float _rotationSharpness = 25;
+    private float rotationSharpness = 25;
 
     [SerializeField]
-    private float _minDistance = 0;
+    private float minDistance = 0;
 
     [SerializeField]
-    private float _maxDistance = 10f;
+    private float maxDistance = 10f;
 
     [SerializeField]
-    private float _defaultDistance = 2f;
-
-    [Header("Obstructions")]
-    [SerializeField]
-    private float _checkRadius = 0.2f;
+    private float defaultDistance = 2f;
 
     [SerializeField]
-    private LayerMask _obstructionLayers;
+    private LayerMask obstructionLayers;
 
-    private List<Collider> _ignoreColliders = new List<Collider>();
+    private List<Collider> ignoredColliders = new List<Collider>();
 
-    private float _targetVerticalAngle;
+    private float targetVerticalAngle;
 
-    private Vector3 _targetPosition;
+    private Vector3 targetPosition;
 
-    private float _targetDistance;
+    private float targetDistance;
 
     public Vector3 cameraPlanarDirection
     {
@@ -62,118 +58,116 @@ public class CameraController : Observer
     }
 
     [SerializeField]
-    private Vector3 _framing;
+    private Vector3 framing;
 
     [SerializeField]
-    private bool _invertX;
+    private bool invertX;
 
     [SerializeField]
-    private bool _invertY;
+    private bool invertY;
 
     [SerializeField]
-    private float _defaultVerticalAngle = 20f;
+    private float defaultVerticalAngle = 20f;
 
-    private Vector3 _newPosition;
+    private Vector3 newPosition;
 
-    private Quaternion _newRotation;
+    private Quaternion newRotation;
 
     private void OnValidate()
     {
-        _defaultDistance = Mathf.Clamp(_defaultDistance, _minDistance, _maxDistance);
-        _defaultVerticalAngle = Mathf.Clamp(
-            _defaultVerticalAngle,
-            _minVerticalAngle,
-            _maxVerticalAngle
+        defaultDistance = Mathf.Clamp(defaultDistance, minDistance, maxDistance);
+        defaultVerticalAngle = Mathf.Clamp(
+            defaultVerticalAngle,
+            minVerticalAngle,
+            maxVerticalAngle
         );
     }
 
     public void adjustCameraForCrouch()
     {
-        _framing.x = -0.02f;
-        _framing.y = -0.69f;
+        framing.x = -0.02f;
+        framing.y = -0.69f;
     }
 
     public void adjustCameraForStanding()
     {
-        _framing.x = 0;
-        _framing.y = 0;
+        framing.x = 0;
+        framing.y = 0;
     }
 
     private void Start()
     {
-        _obstructionLayers = 1 << 2;
-        _obstructionLayers = ~_obstructionLayers;
-        _framing = new Vector3(0, 0, 0);
-        _ignoreColliders.AddRange(GetComponentsInChildren<Collider>());
-        planarDirection = _followTransform.forward;
+        obstructionLayers = 1 << 2;
+        obstructionLayers = ~obstructionLayers;
+        framing = new Vector3(0, 0, 0);
+        ignoredColliders.AddRange(GetComponentsInChildren<Collider>());
+        planarDirection = playerTransform.forward;
 
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.Locked;
-        _targetDistance = _defaultDistance;
-        _targetVerticalAngle = _defaultVerticalAngle;
-        _targetRotation =
-            Quaternion.LookRotation(planarDirection) * Quaternion.Euler(_targetVerticalAngle, 0, 0);
-        _targetPosition =
-            _followTransform.position - (_targetRotation * Vector3.forward) * _targetDistance;
+        targetDistance = defaultDistance;
+        targetVerticalAngle = defaultVerticalAngle;
+        targetRotation =
+            Quaternion.LookRotation(planarDirection) * Quaternion.Euler(targetVerticalAngle, 0, 0);
+        targetPosition =
+            playerTransform.position - (targetRotation * Vector3.forward) * targetDistance;
     }
 
     private void LateUpdate()
     {
-        float _zoom = -PlayerInputs.MouseScrollInput * _zoomSpeed; // -0.5 lub 0.5
+        float zoom = -PlayerInputs.MouseScrollInput * zoomSpeed; // -0.5 lub 0.5
         float mouseX = PlayerInputs.MouseXInput; //wartosc od okolo -40 do 40 - 40 bardzo szybki ruch, 10 srednio szybki,
         float mouseY = -PlayerInputs.MouseYInput;
-        if (_invertX)
+        if (invertX)
         {
             mouseX *= -1;
         }
-        if (_invertY)
+        if (invertY)
         {
             mouseY *= -1;
         }
 
-        Vector3 _focusPosition = _followTransform.position + new Vector3(_framing.x, _framing.y, 0);
+        Vector3 focusPosition = playerTransform.position + new Vector3(framing.x, framing.y, 0);
 
         planarDirection = Quaternion.Euler(0, mouseX, 0) * planarDirection; //rotate planar direction (mouseX degrees) around y axis
-        _targetDistance = Mathf.Clamp(_targetDistance + _zoom, _minDistance, _maxDistance); //zwiekszamy/zmniejszamy dystans o wartosc _zoom
-        _targetVerticalAngle = Mathf.Clamp(
-            _targetVerticalAngle + mouseY,
-            _minVerticalAngle,
-            _maxVerticalAngle
+        targetDistance = Mathf.Clamp(targetDistance + zoom, minDistance, maxDistance); //zwiekszamy/zmniejszamy dystans o wartosc _zoom
+        targetVerticalAngle = Mathf.Clamp(
+            targetVerticalAngle + mouseY,
+            minVerticalAngle,
+            maxVerticalAngle
         );
 
-        float _smallestDistance = _targetDistance;
+        float smallestDistance = targetDistance;
         RaycastHit hit;
         bool didHit = Physics.Raycast(
-            _followTransform.position,
-            -_camera.transform.forward,
+            playerTransform.position,
+            -camera.transform.forward,
             out hit,
-            Vector3.Distance(_camera.transform.position, _followTransform.position),
-            _obstructionLayers
-        ); //cast ray from player model, ray is a sphere, ray toward camera, sphere is moving "_targetDistance" towards destination
-
-        // and has radius = checkRadius
+            Vector3.Distance(camera.transform.position, playerTransform.position),
+            obstructionLayers
+        );
 
         if (didHit)
         {
-            _smallestDistance = hit.distance;
+            smallestDistance = hit.distance;
         }
 
-        _targetRotation =
-            Quaternion.LookRotation(planarDirection) * Quaternion.Euler(_targetVerticalAngle, 0, 0);
-        _targetPosition = _focusPosition - (_targetRotation * Vector3.forward) * _smallestDistance;
-        _newRotation = Quaternion.Slerp(
-            _camera.transform.rotation,
-            _targetRotation,
-            Time.deltaTime * _rotationSharpness
+        targetRotation =
+            Quaternion.LookRotation(planarDirection) * Quaternion.Euler(targetVerticalAngle, 0, 0);
+        targetPosition = focusPosition - (targetRotation * Vector3.forward) * smallestDistance;
+        newRotation = Quaternion.Slerp(
+            camera.transform.rotation,
+            targetRotation,
+            Time.deltaTime * rotationSharpness
         );
-        _newPosition = Vector3.Lerp(
-            _camera.transform.position,
-            _targetPosition,
-            Time.deltaTime * _rotationSharpness
+        newPosition = Vector3.Lerp(
+            camera.transform.position,
+            targetPosition,
+            Time.deltaTime * rotationSharpness
         );
 
-        _camera.transform.position = _newPosition;
-        _camera.transform.rotation = _newRotation;
+        camera.transform.position = newPosition;
+        camera.transform.rotation = newRotation;
     }
 
     public override void OnEvent(EventDTO eventDTO)
