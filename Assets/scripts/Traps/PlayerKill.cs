6195fa7ca8eacb7376f9352292b45822669c34ae
Mateusz <mateusz.pietal@gmail.com>
2024-12-02 20:26:57 +0100

    Make the falling balls defeat player only while moving.

--- Assets/scripts/Traps/PlayerKill.cs
+++ Assets/scripts/Traps/PlayerKill.cs
@@ -7,9 +7,15 @@ public class PlayerKill : MonoBehaviour
 {
     private EventQueue eventQueue;
 
+    private Rigidbody rigidBody;
+
+    [SerializeField]
+    private float minSpeedToDefeatPlayer;
+
     private void Start()
     {
         eventQueue = FindAnyObjectByType<EventQueue>();
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -22,7 +28,11 @@ public class PlayerKill : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.tag.Equals(Tags.PLAYER))
+        if (
+            collision.collider.tag.Equals(Tags.PLAYER)
+            && rigidBody != null
+            && rigidBody.linearVelocity.magnitude > minSpeedToDefeatPlayer
+        )
         {
             eventQueue.SubmitEvent(new EventDTO(EventType.PLAYER_DIED, null));
         }
