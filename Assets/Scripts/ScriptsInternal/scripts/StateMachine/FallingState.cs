40973cc945f75e89434c70867d24b4a8cc484a80
Mateusz <mateusz.pietal@gmail.com>
2025-01-01 15:39:51 +0100

    Revert jump fix, add more map elements.

--- Assets/Scripts/ScriptsInternal/scripts/StateMachine/FallingState.cs
+++ Assets/Scripts/ScriptsInternal/scripts/StateMachine/FallingState.cs
@@ -35,14 +35,6 @@ public class FallingState : MovementState
     public override void FrameUpdate()
     {
         base.Move(characterController.currentVelocity.normalized);
-        if (
-            characterController.IsColliding()
-            && characterController.rigidbody.linearVelocity.magnitude == 0
-        )
-        {
-            stateMachine.ChangeState(stateMachine.runState);
-            characterController.animationsManager.setAnimationToLandingFromStand();
-        }
     }
 
     private bool IsDetectedObjectAWall()
